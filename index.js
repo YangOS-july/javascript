3ed60576a800f8adb93be1d84e44025b73cc6c9f
yang <yang@example.com>
2025-01-08 11:22:31 +0800

    提交js数据类型部分代码

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hhh我的")
+document.write("<h1>Hello World!</h1>");
---
+++ test01.js
@@ -0,0 +1,66 @@
+// let name = "小明";      // 声明一个变量 name 并赋值为“小明”
+// let age  = 11;          // 声明一个变量 age
+// age=12
+// const PI = 3.1415       // 声明一个常量 PI，并赋值为 3.1415
+// console.log(PI)         // 在控制台打印 PI
+
+// var a;                      // 此时 a 为 Undefined
+// console.log(typeof(a))
+// a = "http://www.csdn.net/"; // 此时 a 为 String 类型
+// console.log(typeof(a))
+// a = 123;                    // 此时 a 为 Number 类型
+// console.log(typeof(a))
+
+// var str = "Let's have a cup of coffee.";  // 双引号中包含单引号
+// console.log(str+typeof(str))
+// var str = 'He said "Hello" and left.';    // 单引号中包含双引号
+// console.log(str+typeof(str))
+// var str = "We\'ll never give up.";        // 使用反斜杠转义字符串中的单引号
+// console.log(str+typeof(str))
+
+
+// var num1 = 123;     // 整数
+// console.log(typeof(num1))
+// var num2 = 3.14;    // 浮点数
+// console.log(typeof(num2))
+
+
+// var person = {
+//     name: 'Bob',
+//     age: 20,
+//     tags: ['js', 'web', 'mobile'],
+//     city: 'Beijing',
+//     hasCar: true,
+//     zipcode: null
+// };
+// console.log(person.name);       // 输出 Bob
+// console.log(typeof(person));        // 输出 20
+
+// var arr = [1, 2, 3, 'hello', true, null]
+// console.log(typeof(arr))
+
+// function sayHello(name){
+//     return "Hello, " + name;
+// }
+// var res = sayHello("Peter");
+// console.log(res);  // 输出 Hello, Peter
+
+// var fun = function(){
+//     console.log("http://www.csdn.net/js/");
+// }
+// function createGreeting(name){
+//     return "Hello, " + name;
+// }
+// function displayGreeting(greetingFunction, userName){
+//     return greetingFunction(userName);
+// }
+// var result = displayGreeting(createGreeting, "Peter");
+// console.log(result);  // 输出 Hello, Peter
+
+
+
+
+
+
+
+
