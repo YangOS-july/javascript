e608d3ece3af4de7b456d07476f03e04364419ca
yang <yang@example.com>
2025-01-09 15:59:34 +0800

    提交js部分代码

--- test01.js
+++ test01.js
@@ -58,9 +58,123 @@
 // console.log(result);  // 输出 Hello, Peter
 
 
+// var x = "Hello ";
+// var y = "World!";
+// var z = x + y;
+// console.log(z);  // 输出：Hello World!
+// x += y;
+// console.log(x);  // 输出：Hello World!
+
+
+// var id = 3;
+// switch (id) {
+//     default : 
+//     console.log("游客");
+//     break
+//     case 1 :
+//         console.log("普通会员");
+//         break;
+//     case 2 :
+//         console.log("VIP会员");
+//         break;
+
+   
+// }
+
+// console.log(id)
+
+// var id = 4;
+// switch (id) {
+//     case 1 :
+//         console.log("普通会员");
+//         break;  //停止执行，跳出switch
+//     case 2 :
+//         console.log("VIP会员");
+//         break;  //停止执行，跳出switch
+//     case 3 :
+//         console.log("管理员");
+//         break;  //停止执行，跳出switch
+//     default :   //上述条件都不满足时，默认执行的代码
+//         console.log("游客");
+// }
+
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i + " ");
+// }
+
+// var z = 0;
+// for (;;) {
+//     if(z > 5){
+//         break;
+//     }
+//     // 要执行的代码
+//     z++;
+// }
+
+// console.log(z)
+
+
+// function getSum(num1, num2){
+//     return num1 + num2;
+// }
+// var sum1 = getSum(7, 12);      // 函数返回值为：19
+// var sum2 = getSum(-5, 33);     // 函数返回值为：28
+
+// console.log(sum1)
+// console.log(sum2)
+
+
+// 函数表达式
+// var getSum = function(num1, num2) {
+//     var total = num1 + num2;
+//     return total;
+// };
+
+// console.log(getSum(1,2))
+
+
+// var x = 123;    // 整数
+// var y = 3.14;   // 浮点数
+// var z = 0xf;   // 十六进制数：255
+
+
+// console.log(z)
+
+
+// var x = 1.57e4;     // 等于 15700
+// var y = 4.25e+6;    // 等于 4250000
+// var z = 4.25e-6;    // 等于 0.00000425
+
+// console.log(new Date().toLocaleString())
+
+
+        // function fn(a, b, ...c) {//剩余参数只能写在参数的最后面
+        //     console.log(a + b, c) //c:真数据（数组）
+        // }
+        // fn(1, 2, 3, 4, 5)
 
 
 
 
+        // const arr = [1, 2, 3]
+        // console.log(...arr)
+
+        // const obj = {
+        //     uname: 'niuniu',
+        //     age: 18
+        // }
+        // console.log({...obj})
 
 
+
+
+        function getSum(x,y) {
+            //逻辑中断：如果 实参 没有传递给形参 x 和 y ，则会返回 0，而不是NaN
+            x = x || 0
+            y = y || 0
+            console.log(x,y)
+        }
+        getSum(2) //2 0  => 如果没有逻辑中断，则返回 NaN
+        getSum(3,6) //3 6
+        
